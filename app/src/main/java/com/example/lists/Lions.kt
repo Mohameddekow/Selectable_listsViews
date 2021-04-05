512c0b51b7b2e27f73874de8788c40ec7248e250
mohamed dekow <mohameddekow77@gmail.com>
2021-04-05 12:41:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lists/Lions.kt
@@ -0,0 +1,11 @@
+package com.example.lists
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moha)
+    }
+}
---
+++ app/src/main/java/com/example/lists/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lists
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lists = arrayOf("lions", "cats", "pets","shops", "wholesales"," retails", "animals")
+
+        listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lists)
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+
+//            start a new activity on clicking on the first item
+            if(position == 0){
+                startActivity(Intent(applicationContext, Lions::class.java))
+            }
+            Toast.makeText(applicationContext, "you clicked " + lists[position], Toast.LENGTH_SHORT).show()
+        }
+    }
+}
